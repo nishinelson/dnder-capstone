a87bf30c93506ac33f3320a83c624a6cbbf72c34
Nishi <naalnishinelson@gmail.com>
2021-05-04 23:16:03 -0700

    model updates

--- dnder-capstone-app/app/models/user.py
+++ dnder-capstone-app/app/models/user.py
@@ -2,6 +2,7 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
   __tablename__ = 'users'
 
@@ -14,7 +15,7 @@ class User(db.Model, UserMixin):
   bio = db.Column(db.String(2000))
   hashed_password = db.Column(db.String(255), nullable=False)
   pc = db.relationship("PC", uselist=False, back_populates="user")
-
+  dm = db.relationship("DM", uselist=False, back_populates="user")
 
   @property
   def password(self):
@@ -38,8 +39,25 @@ class User(db.Model, UserMixin):
       "email": self.email,
       "city": self.city,
       "state": self.state,
-      "bio": self.satte
+      "bio": self.bio
     }
+
+
+party = db.Table(
+  "party",
+  db.Column(
+    "dmId",
+    db.Integer,
+    db.ForeignKey("DMs.id"),
+    primary_key=True
+  ),
+  db.Column(
+    "pcId",
+    db.Integer,
+    db.ForeignKey("PCs.id"),
+    primary_key=True
+  )
+)
 
 
 class PC(db.Model):
@@ -50,15 +68,20 @@ class PC(db.Model):
   experience = db.Column(db.String(25), nullable=False)
   description = db.Column(db.String(2000))
   userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  user = db.relationship("User", back_populates="pc")
+  user = db.relationship('User', back_populates='pc')
+  dms = db.relationship("DM", secondary=party, back_populates="PCs")
 
 
 class DM(db.Model):
   __tablename__ = 'DMs'
 
   id = db.Column(db.Integer, primary_key=True)
-  pcClass = db.Column(db.String(40), nullable=False)
+  campaign = db.Column(db.String(255), nullable=False)
+  resources = db.Column(db.String(3000), nullable=False)
   experience = db.Column(db.String(25), nullable=False)
+  partySize = db.Column(db.String(25), nullable=False)
+  groupType = db.Column(db.String(25), nullable=False)
   description = db.Column(db.String(2000))
   userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  user = db.relationship("User", back_populates="pc")
+  user = db.relationship('User', back_populates='dm')
+  pcs = db.relationship("PC", secondary=party, back_populates="DMs")
