480da685cdb0e3c2c2a584c0680107d5684b4fa8
Nishi <naalnishinelson@gmail.com>
2021-06-24 16:19:12 -0700

    carousel implemented!

--- dnder-capstone-app/react-app/src/components/Splash/index.js
+++ dnder-capstone-app/react-app/src/components/Splash/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import './Splash.css';
+import Carousel from "../Carousel";
 
 const Splash = () => {
   const user = useSelector(state => state.session.user)
@@ -16,10 +17,11 @@ const Splash = () => {
       <div className='splashInfo'>Create a player and/or dungeon master card and swipe through other
         user's cards in order to match with people you'd like to play Dungeons and Dragons with.
       </div>
-      <div className='splashMessage'>Log-in or Sign-up to start swiping for your dnd party!</div>
+      {/* <div className='splashMessage'>Log-in or Sign-up to start swiping for your dnd party!</div> */}
       <div className='intro'>Here's how to navigate this site:</div>
-      <div className='first-instr'>After Logging in or Signing up, press the button at the center of either card to create your own PC </div>
-      <img className='instruct-img-1' src='https://i.imgur.com/Ad74PxS.png' alt='first walkthrough img'/>
+      <Carousel/>
+      {/* <div className='first-instr'>After Logging in or Signing up, press the button at the center of either card to create your own PC </div>
+      <img className='instruct-img-1' src='https://i.imgur.com/Ad74PxS.png' alt='first walkthrough img'/> */}
 
     </div>
   )
